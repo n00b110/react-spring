a6d5dddb5a7963da763d0701a2a645e71f9b6fea
Immanuel Olaoye <olaoye939@gmail.com>
2023-12-11 21:42:34 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Component, useState } from 'react'
+import Component1 from './components/Component1'
+import './App.css'
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Component1></Component1>
+      </div>
+    )
+  }
+}
+
+
+
+export default App;
+
---
+++ src/components/Component1.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Spring } from "react-spring";
+
+export default function Component1(){
+    return (
+        <div style={c1Style}>
+            <h1>Component1</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                Ullam architecto sequi deserunt cupiditate tempore quia dolorum vero accusantium natus, corporis soluta. 
+                Adipisci beatae facilis commodi sit reiciendis soluta non fugit.</p>
+        </div>
+    );
+}
